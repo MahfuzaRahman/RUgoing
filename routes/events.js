9d9d90aa880bc9e1335f99745bb22e7e2b81e7ba
Mahfuza Rahman <mahfuzarahman@Mahfuzas-MBP.home>
2022-04-28 00:25:53 -0400

    created more funcs

--- routes/events.js
+++ routes/events.js
@@ -25,43 +25,24 @@ let clubs = [];
     var json = JSON.stringify(items, null, 2);
     fs.writeFileSync("events.json", json);
 
+    // add all events and clubs to respective arrays
     for(var i=0; i<items.length; i++){
-        /**
-         * Creates an event object to add to events[] that stores event information and club information
-         */
-        let clubInfo = (String)(items[i].author);
-        let parIndex = clubInfo.indexOf('(');
-        let event = createEvent(items[i])
-        events.push(event);
-        
-        /**
-         * Creates a club object to add to clubs[] if not already in clubs
-         */
-        let club = {
-            "clubEmail": clubInfo.substring(0, parIndex),
-            "clubName": clubInfo.substring(parIndex+1, clubInfo.length-1),
-            events: []
-        }
-        if(!clubs.some(c => c.clubName === club.clubName)) {
-            club.events.push(event);
-            clubs.push(club);
-        } else {
-            clubs.find(c => c.clubName === club.clubName).events.push(event);
-        }
+        let item = items[i];
+        let event = createEvent(item);        
+        let club = createClub(item);
+        addEvent(event);
+        addClub(club, event);
     }
     
-    /**
-     * Loops through clubs[] and prints the name of the club and their events
-     */
     //let numEvents = 0;
     for(var k = 0; k <clubs.length; k++){
         console.log(k + " " + clubs[k].clubName);
         for(var i = 0; i < clubs[k].events.length; i++){
             console.log(clubs[k].events[i].eventName);
-            numEvents++;
+           // numEvents++;
         }
     }    
-   // console.log(numEvents + " " + events.length);
+    // console.log(numEvents + " " + events.length);
 
 })();
 
@@ -88,16 +69,46 @@ function createEvent(item){
     return event;
 }
 
-
-function createClub()
-
+/**
+ * Creates club by adding information parsed
+ * from RSS feed
+ * @param {*} item array element of array events on RSS feed
+ * @returns event
+ */
+function createClub(item){
+    let clubInfo = (String)(item.author);
+    let parIndex = clubInfo.indexOf('(');
+    let club = {
+        "clubEmail": clubInfo.substring(0, parIndex),
+        "clubName": clubInfo.substring(parIndex+1, clubInfo.length-1),
+        events: []
+    }
+    return club;
+}
 
 /**
- * Gets user listings
+ * Adds events to events []
+ * @param {*} event event to add to events []
  */
-router.get('/getEvents', function(req, res, next) {
-    res.send(events);
-});
+function addEvent(event){
+    events.push(event);
+}
+
+/**
+ * Adds club to clubs [] and adds respective events to given club
+ * @param {*} club club to add to clubs []
+ */
+function addClub(club, event){
+    // Checks if club already exists
+    if(!clubs.some(c => c.clubName === club.clubName)) {
+        // If club is not in array, add event to club's events [], and then add club
+        club.events.push(event);
+        clubs.push(club);
+    } else {
+        // If club is in array, add event to club's events []
+        clubs.find(c => c.clubName === club.clubName).events.push(event);
+    }
+}
 
 /**
  * Parses through description to find the date of the event
@@ -108,6 +119,13 @@ function getDate(description){
     let fields = description.split('\r\n');
     return fields[fields.length - 3];
 }
+
+/**
+ * Gets user listings
+ */
+ router.get('/getEvents', function(req, res, next) {
+    res.send(events);
+});
 
 /**
  *  Searches for events given a club name
